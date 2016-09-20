50d0b1b8d1365c161b04ddcd56e83b57d5352336
Johan Guldmyr <johan.guldmyr@csc.fi>
2016-09-20 10:26:26 +0300

    testing: cat /etc/chrony.conf

--- tests/test-in-docker-image.sh
+++ tests/test-in-docker-image.sh
@@ -119,7 +119,8 @@ function test_playbook(){
 }
 function extra_tests(){
 
-    ${APACHE_CTL} configtest || (echo "php --version was failed" && exit 100 )
+	echo "TEST: cat chrony.conf"
+	cat /etc/chrony.conf
 }
 
 
@@ -134,7 +135,7 @@ function main(){
     test_playbook_syntax
     test_playbook
     test_playbook_check
-#    extra_tests
+    extra_tests
 
 }
 
